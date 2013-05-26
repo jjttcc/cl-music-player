a980f28c966dfe4477f692968687b829b228fae8
Jim Cochrane <jim.cochrane@gmail.com>
2013-05-26 16:23:45 -0500

    Added (hard-coded for now) no-loop, no-repeat
    Added --no-loop, --no-repeat options to vlc command.

--- player.rb
+++ player.rb
@@ -2,13 +2,13 @@ class Player
   include Process
 
   def playfile(path)
-    command = "vlc '#{path}'"
+    command = "vlc --no-loop --no-repeat '#{path}'"
     pid = fork { exec command }
 #    waitpid(pid)
   end
 
   def playfiles(list)
-    command = 'vlc '
+    command = 'vlc --no-loop --no-repeat '
     list.each do |f|
       command += "\"#{f}\" "
     end
